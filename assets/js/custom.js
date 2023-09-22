e4bb48a38586a4e9364c9529e22048f997ecffad
surojit saha <surojitsaha729@gmail.com>
2023-09-23 01:29:36 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,12 +23,12 @@ jQuery(document).ready(function () {
   )
   // Sidebar Dropdown
   jQuery(".sidebar-menu-outer li.menu-item-has-children > a").click(function () {
-    jQuery('.sidebar-menu-outer .sub-menu').slideUp();
+    jQuery('.sidebar-menu-outer .sub-menu').slideUp(300);
     if (jQuery(this).parent().find(".sub-menu").css('display') == 'none') {
-      jQuery(this).parent().find(".sub-menu").slideDown();
+      jQuery(this).parent().find(".sub-menu").slideDown(300);
       jQuery("body").addClass("sub_menu_active");
     } else {
-      jQuery(this).parent().find(".sub-menu").slideUp();
+      jQuery(this).parent().find(".sub-menu").slideUp(300);
     }
   });
 
@@ -48,12 +48,13 @@ jQuery(document).ready(function () {
         '<div class="tooltip navbar-sidenav-tooltip bs-tooltip-right" role="tooltip" style="pointer-events: none;"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
     })
   })
+  // Toggle sidebar when the button is clicked
+  jQuery(".sidebar-toggle-btn").on("click", function () {
+    jQuery("body").toggleClass("sidebar-collapsed");
+    jQuery(".sidebar-menu-outer .sub-menu").css("display", "none");
+  })
+  // Datatabel Initialization
+  new DataTable('#example');
 })
 
-jQuery(document).ready(function () {
-  // Toggle sidebar when the button is clicked
-  jQuery("#sidebarCollapse").on("click", function () {
-    jQuery("#sidebar").toggleClass("active")
-    jQuery("#content").toggleClass("active")
-  })
-})
+
